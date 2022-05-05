331de1027a801555a4bfa8a8f4a1c9b1d858f70f
Yao Yu <amyyuyao.thu@gmail.com>
2022-05-04 21:42:48 -0400

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,27 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id              INT unsigned NOT NULL AUTO_INCREMENT,
+  username        INT NOT NULL,
+  PRIMARY KEY     (id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id              INT unsigned NOT NULL AUTO_INCREMENT,
+  message         VARCHAR(150) NOT NULL,
+  roomname        VARCHAR(20) NOT NULL,
+  username_id     INT unsigned,
+  PRIMARY KEY     (id),
+  FOREIGN KEY (username_id)    REFERENCES users(id)
 );
+
+
 
 /* Create other tables and define schemas for them here! */
 
